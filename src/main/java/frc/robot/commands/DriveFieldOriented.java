fb149136a944610ef4a8829ab260f20fa0ce9123
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-01-20 12:21:51 -0600

    testing not scaling controller right axis

--- src/main/java/frc/robot/commands/DriveFieldOriented.java
+++ src/main/java/frc/robot/commands/DriveFieldOriented.java
@@ -23,7 +23,7 @@ public class DriveFieldOriented extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drive.driveFieldOriented(RobotContainer.getScaledControllerLeftYAxis(), RobotContainer.getScaledControllerLeftXAxis(), RobotContainer.getScaledControllerRightXAxis());
+    drive.driveFieldOriented(RobotContainer.getScaledControllerLeftYAxis(), RobotContainer.getScaledControllerLeftXAxis(), RobotContainer.getControllerRightXAxis());
   }
 
   // Called once the command ends or is interrupted.
